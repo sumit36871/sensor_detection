5bcd7c1e56b3509e3220345811de32d7b7252ac6
sumit yadav <sumitbsc36871@gmail.com>
2024-04-19 10:20:06 +0530

    folder structure updated

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from sensor.exception import SensorException
 import sys
 import os
 from sensor.logger import logging 
+from sensor.utils import dump_csv_file_to_mongodb_collection
 
-
-def test_exception():
-    try:
-        logging.info("ki yaha pe bhahiya ek error ayegi devision by zero wali error")
-        a=1/0
-    except Exception as e:
-        raise SensorException(e,sys)
+#def test_exception():
+#  try:
+#    logging.info("ki yaha pe bhahiya ek error ayegi devision by zero wali error")
+#   a=1/0
+#except Exception as e:
+#    raise SensorException(e,sys)
 
 
 if __name__ == '__main__':
-    try:
-        test_exception()
-    except SensorException as e:
-        print(e)
+    file_path = "C:/Projects/Sensor_detection/aps_failure_training_set1.csv"
+    database_name = "APS_detection"
+    collection_name = "sensor"
+    dump_csv_file_to_mongodb_collection(file_path,database_name,collection_name)
+
+    
+#try:
+#test_exception()
+#except SensorException as e:
+#  print(e)
